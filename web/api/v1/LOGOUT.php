fce5692f7835531bf15f19348e2bdd2dd1bb4587
Brendan Connelly <brendanjconnelly17@gmail.com>
2024-09-01 16:48:40 -0400

    i lied now it works (i forgor the -> operatoR)

--- web/api/v1/LOGOUT.php
+++ web/api/v1/LOGOUT.php
@@ -4,7 +4,10 @@ require_once getenv("PHP_ROOT") . "/api/helper/USER_AUTH.php";
 $data = json_decode(file_get_contents("php://input"));
 
 if(isset($data->username) && isset($data->token)) {
-    deleteToken($username, $token);
+    deleteToken($data->username, $data->token);
+    echo '0';
+    exit();
 }
+echo '1';
 
 ?>
